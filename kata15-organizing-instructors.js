639a18a672c86ba3a0b1024f66c4c706fb7f0bdb
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-06-18 19:41:21 +0000

    initial commit

---
+++ kata15-organizing-instructors.js
@@ -0,0 +1,24 @@
+const organizeInstructors = function (instructors) {
+  let sorted = {};
+  for (let instructor of instructors) {
+    if (!sorted[instructor.course]) {
+      sorted[instructor.course] = [];
+    }
+    sorted[instructor.course].push(instructor.name);
+  }
+
+  return sorted;
+};
+
+console.log(organizeInstructors([
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
